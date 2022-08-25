912fb54852f0fe3c2bc1bfd2de5b843dc0616c16
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2022-08-25 15:00:59 +0430

    Add Todo Backend
    Reactjs
    VueJS

---
+++ vuejs-todo/client/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="ani-slideInDown text-center">
+		<h2 class="fs-h2">
+			Taha Amin Ghafuri
+		</h2>
+
+		<p>
+			<a href="https://github.com/tahaghafuri" rel="noopener noreferer">
+				https://github.com/tahaghafuri
+			</a>
+		</p>
+	</div>
+</template>
